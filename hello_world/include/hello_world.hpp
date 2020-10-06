c49805b0be222c90efb0f648985c17b9f3e78307
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-10-05 23:00:41 -0700

    First init

---
+++ hello_world/include/hello_world.hpp
@@ -0,0 +1,13 @@
+
+class HelloWorld
+{
+    public:
+        HelloWorld( int x ) :
+            _x(x){}
+
+        int val()( return _x );
+        int incrementX();
+
+    private:
+        int _x;
+};
---
+++ hello_world/src/hello_world.cpp
@@ -0,0 +1,6 @@
+#include "hello_world.hpp"
+
+int HelloWorld::incrementX( int n )
+{
+    return _x+=n;
+}

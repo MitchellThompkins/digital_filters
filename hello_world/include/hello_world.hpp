563dee712cd9393f2a425cf38013fec7da2025ce
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-10-06 22:01:16 -0700

    fixed .hpp

--- hello_world/include/hello_world.hpp
+++ hello_world/include/hello_world.hpp
@@ -1,17 +1,18 @@
 #ifndef HELLO_WORLD_HPP
 #define HELLO_WORLD_HPP
 
-class HelloWorld
-{
-    public:
-        HelloWorld( int x ) :
-            _x(x){}
+class HelloWorld {
+public:
+    HelloWorld(int x)
+        : _x(x)
+    {
+    }
 
-        int val(){ return _x; }
-        int increment( int n=1 );
+    int val() { return _x; }
+    int increment(int n = 1);
 
-    private:
-        int _x;
+private:
+    int _x;
 };
 
 #endif
